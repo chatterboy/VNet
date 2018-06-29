c6ad745c54655a0bc033452e1a13ad77c29b0b4f
chatterboy <chatter0502@gmail.com>
2018-06-29 19:35:15 +0900

    Add two methods in layer.py with conv3d() and deconv3d()

---
+++ src/utility/layer.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+
+BATCH_SIZE = 2
+
+def conv3d(name, input, filter, ksz, stride, padding='SAME', relu=None):
+    with tf.variable_scope(name):
+        iShape = input.get_shape().as_list()
+        kShape = [0] * 5
+        kShape[1:4] = [ksz] * 3 if type(ksz).__name__ == 'int' else ksz
+        sShape = [0] * 5
+        sShape[1:4] = [stride] * 3 if type(stride).__name__ == 'int' else stride
+        W = tf.get_variable('W', shape=[kShape[1], kShape[2], kShape[3], iShape[-1], filter],
+                            dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+        b = tf.get_variable('b', shape=[filter], dtype=tf.float32,
+                            initializer=tf.contrib.layers.xavier_initializer())
+        out = tf.nn.conv3d(input, filter=W, strides=sShape, padding=padding) + b
+        if relu:
+            return relu(out)
+        return out
+
+def deconv3d(name, input, filter, ksz, stride, padding='SAME', relu=None):
+    with tf.variable_scope(name):
+        iShape = input.get_shape().as_list()
+        kShape = [0] * 5
+        kShape[1:4] = [ksz] * 3 if type(ksz).__name__ == 'int' else ksz
+        sShape = [0] * 5
+        sShape[1:4] = [stride] * 3 if type(stride).__name__ == 'int' else stride
+        W = tf.get_variable('W', shape=[kShape[1], kShape[2], kShape[3], filter, iShape[-1]],
+                            dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+        b = tf.get_variable('b', shape=[filter], dtype=tf.float32,
+                            initializer=tf.contrib.layers.xavier_initializer())
+        out = tf.nn.conv3d_transpose(input, filter=W,
+                                     output_shape=tf.concat([BATCH_SIZE, sShape[1] * iShape[1],
+                                                             sShape[2] * iShape[2],
+                                                             sShape[3] * iShape[3], filter]),
+                                     strides=sShape, padding=padding) + b
+        if relu:
+            return relu(out)
+        return out
