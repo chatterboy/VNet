85c497fac2deeea2890910e09997df1ed9413f3e
chatterboy <chatter0502@gmail.com>
2018-06-29 18:21:34 +0900

    Add two methods in loss.py with diceCoef() and diceLoss()

---
+++ src/utility/loss.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+def diceCoef(logit, label):
+    logit = tf.reshape(logit, [-1, 2])
+    label = tf.reshape(label, [-1, 2])
+    a = 2 * tf.reduce_sum(tf.multiply(logit, label))
+    b = tf.reduce_sum(tf.square(logit) + tf.square(label))
+    return
+
+def diceLoss(logit, label):
+        with tf.variable_scope('dice_loss'):
+            return 1 - diceCoef(logit, label)
