e78519dd6f969e18f7531d6084b814fcddfed84c
chansik <chatter0502@gmail.com>
2018-06-29 18:17:36 +0900

    Re-add 'Batch' class in dm.py (modification for some methods)

--- src/utility/dm.py
+++ src/utility/dm.py
@@ -16,15 +16,16 @@ class Batch:
         self.y = y
         self.batch_size = batch_size
         self.index = 0
-        self.iList = np.array([i for i in range(self.x.shape[0])])
 
     def reachToLast(self):
         return self.x.shape[0] < self.index + self.batch_size
 
+
     def nextTo(self):
         if self.reachToLast():
             self.index = 0
-            np.random.shuffle(self.iList)
-        batch_x = self.getBatch(self.x, self.iList[self.index : self.index + self.batch_size])
-        batch_y = self.getBatch(self.y, self.iList[self.index : self.index + self.batch_size])
+            np.random.shuffle(self.x)
+            np.random.shuffle(self.y)
+        batch_x = self.x[self.index : self.index + self.batch_size]
+        batch_y = self.y[self.index : self.index + self.batch_size]
         return batch_x, batch_y
