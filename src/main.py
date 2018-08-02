e48f80a0f3d3d67d520b13617b5769f5dacdec10
chatterboy <chatter0502@gmail.com>
2018-08-02 17:02:36 +0900

    Add in train() that show images from the model

--- src/main.py
+++ src/main.py
@@ -6,16 +6,17 @@ from src.network.vnet import VNet
 config = dict()
 
 # This can be done by a user want to use VNetx
-config['epochs'] = 20
+config['epochs'] = 3000
 config['batch_size'] = 2
 config['learning_rate'] = 0.0001
 config['momentum'] = 0.99
 config['epoch_step'] = 5
+config['valid_step'] = 1500
 config['base_path'] = os.path.abspath('..')
 config['train_path'] = 'data/train'
 config['test_path'] = 'data/test'
 config['chks_path'] = 'checkpoint'
-config['flag'] = 'retrain' # train, retrain, test
+config['flag'] = 'train' # train, retrain, test
 
 if config['flag'] == 'train':
     chksPath = os.path.join(config['base_path'], 'checkpoint')
