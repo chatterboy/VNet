90212cccb8a6616c9394c2f2eb3ef67ae959a94c
chatterboy <chatter0502@gmail.com>
2018-08-06 15:04:32 +0900

    Modify DataManager for simple access

--- src/utility/dm.py
+++ src/utility/dm.py
@@ -22,34 +22,32 @@ class Batch:
         return batch_x, batch_y
 
 class DataManager:
-    """
-
-    """
-    def __init__(self, config):
+    def __init__(self, paths):
         """
-
+            There are some paths as well as for train data
+        :param paths: dictionary,
         """
-        self.config = config
+        self._paths = paths
 
-    def read_image_files(self, path):
+    def _read_image_files(self, path):
         """
         :param path: string, the path of the directory includes image files
         :return self.image_files: list of string, a list of image files (names)
         """
-        self.image_files = [f for f in os.listdir(path) if 'raw' not in f and 'segmentation' not in f]
-        return self.image_files
+        self._image_files = [f for f in os.listdir(path) if 'raw' not in f and 'segmentation' not in f]
+        return self._image_files
 
-    def read_label_files(self, path):
+    def _read_label_files(self, path):
         """
         :param path: string, the path of the directory includes image files
         :return self.label_files: list of string, a list of label files (names)
         """
-        self.label_files = []
-        for name, ext in [os.path.splitext(f) for f in self.read_image_files(path)]:
-            self.label_files.append(''.join([name, '_segmentation', ext]))
-        return self.label_files
+        self._label_files = []
+        for name, ext in [os.path.splitext(f) for f in self._read_image_files(path)]:
+            self._label_files.append(''.join([name, '_segmentation', ext]))
+        return self._label_files
 
-    def read_sitk_image(self, path, dtype=sitk.sitkFloat32):
+    def _read_sitk_image(self, path, dtype=sitk.sitkFloat32):
         """
         :param path:
         :param dtype:
@@ -57,44 +55,45 @@ class DataManager:
         """
         return sitk.Cast(sitk.ReadImage(path), dtype)
 
-    def read_sitk_images(self, path, files, dtype=sitk.sitkFloat32):
+    def _read_sitk_images(self, path, files, dtype=sitk.sitkFloat32):
         """
         :param path: string, the path of the driectory to read the image files
         :param files: list of string, a list of image files (names)
         :param dtype:
         :return self.sitk_images: list of sitk_images
         """
-        self.sitk_images = [self.read_sitk_image(os.path.join(path, f), dtype) for f in files]
-        return self.sitk_images
+        self._sitk_images = [self._read_sitk_image(os.path.join(path, f), dtype) for f in files]
+        return self._sitk_images
 
-    def read_sitk_labels(self, path, files, dtype=sitk.sitkFloat32):
+    def _read_sitk_labels(self, path, files, dtype=sitk.sitkFloat32):
         """
         :param path:
         :param files:
         :param dtype:
         :return:
         """
-        self.sitk_labels = [self.read_sitk_image(os.path.join(path, f), dtype) for f in files]
-        return self.sitk_labels
+        self._sitk_labels = [self._read_sitk_image(os.path.join(path, f), dtype) for f in files]
+        return self._sitk_labels
 
     def load_train_data(self):
         """
-            Load train data from the path specified, the data consists of images and ground-truth to train a model
+            Load train data from the path specified, the data consists of images and ground-truths to train a model
         :return:
         """
-        train_data_path = os.path.join(self.config['base_path'], self.config['train_path'])
-        image_files = self.read_image_files(train_data_path)
-        label_files = self.read_label_files(train_data_path)
-        return self.read_sitk_images(train_data_path, image_files), self.read_sitk_labels(train_data_path, label_files)
+        train_data_path = self._paths['train_data_path']
+        image_files = self._read_image_files(train_data_path)
+        label_files = self._read_label_files(train_data_path)
+        self._read_sitk_images(train_data_path, image_files)
+        self._read_sitk_labels(train_data_path, label_files)
 
     def get_sitk_images(self):
-        return self.sitk_images
+        return self._sitk_images
 
     def get_sitk_labels(self):
-        return self.sitk_labels
+        return self._sitk_labels
 
     def get_numpy_images(self):
-        return [sitk.GetArrayFromImage(img) for img in self.sitk_images]
+        return [sitk.GetArrayFromImage(img) for img in self._sitk_images]
 
     def get_numpy_labels(self):
-        return [sitk.GetArrayFromImage(gt) for gt in self.sitk_labels]
+        return [sitk.GetArrayFromImage(gt) for gt in self._sitk_labels]
